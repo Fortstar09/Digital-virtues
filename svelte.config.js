35fed393ca72da3c317ba6b3e8335009f285d85c
Sascha Goebel <git@saschagoebel.com>
2024-08-12 18:40:07 +0200

    feat(VENTURES-111): svelte-preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
     preprocess: [
-        vitePreprocess(),
+        sveltePreprocess({
+            replace: [[/process\.env\.NODE_ENV/g, JSON.stringify(process.env.NODE_ENV)]],
+        }),
         mdsvex({
             layout: {
                 page: 'src/lib/mdsvex/page.svelte',
