d5ccb0966fa927cec845ea82d0d3d3f69383f3ce
Sascha Goebel <git@saschagoebel.com>
2025-04-02 17:18:17 +0200

    Remove potential footer content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,13 +13,12 @@
 </script>
 
 <svelte:head>
-    <link rel="apple-touch-icon" sizes="180x180" href="{base}/apple-touch-icon.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="{base}/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="{base}/favicon-16x16.png" />
-    <link rel="manifest" href="{base}/site.webmanifest" />
-    <link rel="mask-icon" href="{base}/safari-pinned-tab.svg" color="#5bbad5" />
-    <meta name="msapplication-TileColor" content="#da532c" />
-    <meta name="theme-color" content="#ffffff" />
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Digital Virtues GmbH" />
+    <link rel="manifest" href="/site.webmanifest" />
 
     <meta
         name="description"
@@ -35,8 +34,6 @@
         </a>
         <ul role="list">
             <li><a href="{base}/#mission">Mission</a></li>
-            <!-- <li><a href="{base}/#vision">Vision</a></li>
-            <li><a href="{base}/#values">Values</a></li> -->
             <li><a href="{base}/#team">Team</a></li>
             <li><a href="{base}/#references">References</a></li>
             <li><a href="{base}/#timeline">Timeline</a></li>
@@ -64,28 +61,29 @@
         <div class="social">
             <h2>Socials</h2>
             <ul>
-                <li><a href="{base}/">Twitter / X</a></li>
-                <li><a href="{base}/">LinkedIn</a></li>
-                <li><a href="{base}/">Instagram</a></li>
+                <!-- <li><a href="{base}/">Twitter / X</a></li> -->
+                <li><a href="https://www.linkedin.com/company/digitalvirtues/">LinkedIn</a></li>
+                <!-- <li><a href="{base}/">Instagram</a></li>
                 <li><a href="{base}/">Facebook</a></li>
                 <li><a href="{base}/">TikTok</a></li>
                 <li><a href="{base}/">YouTube</a></li>
                 <li><a href="{base}/">Discord</a></li>
                 <li><a href="{base}/">Telegram</a></li>
                 <li><a href="{base}/">Matrix</a></li>
-                <li><a href="{base}/">Mastodon</a></li>
+                <li><a href="{base}/">Mastodon</a></li> -->
             </ul>
         </div>
         <div class="project">
             <h2>Projects</h2>
             <ul>
-                <li><a href="{base}/">Proxy Manager</a></li>
+                <!-- <li><a href="{base}/">Proxy Manager</a></li>
                 <li><a href="{base}/">Traffic Broker</a></li>
                 <li><a href="{base}/">Knowledge Collector</a></li>
                 <li><a href="{base}/">Creator Cartel</a></li>
-                <li><a href="{base}/">Trading AI</a></li>
-                <li><a href="{base}/">Lichtmatrosen</a></li>
-                <li><a href="{base}/">Peach & Lime</a></li>
+                <li><a href="{base}/">Trading AI</a></li> -->
+                <li><a href="{base}/anyblock-analytics/">Anyblock Analytics GmbH</a></li>
+                <li><a href="https://www.dielichtmatrosen.de/">Lichtmatrosen</a></li>
+                <li><a href="https://peachandlime.shop/">Peach & Lime</a></li>
             </ul>
         </div>
     </div>
